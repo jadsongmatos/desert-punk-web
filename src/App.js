b15c9adf9affb6a7a6782aef9be2dfdb9d87efe8
jadsongmatos <jadson.g-matos@outlook.com>
2022-08-05 21:52:34 +0000

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  let size_c = (window.visualViewport.height * 10) / 100;
+  const [x_c, set_x_c] = useState(100);
+  const [y_c, set_y_c] = useState(100);
+
+  const [styles_c, set_styles_c] = useState({
+    transform: `translateX(${x_c}px)`,
+  });
+
+  function click_map(e) {
+    console.log(e.clientY,size_c);
+    set_x_c(e.clientX);
+    set_y_c(e.clientY);
+    set_styles_c({
+      transform: `translate(${e.clientX - (size_c / 2)}px,${e.clientY - size_c}px)`,
+    });
+  }
+
+  useEffect(() => {
+    size_c = (window.visualViewport.height * 10) / 100;
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <p>header</p>
       </header>
+      <div>
+        <img
+          onClick={click_map}
+          draggable="false"
+          className="map position-fixed top-50 start-50 translate-middle"
+          src="/map-t.jpg"
+        ></img>
+        <img
+          style={styles_c}
+          className="character position-fixed"
+          src="/character-p.png"
+        ></img>
+      </div>
     </div>
   );
 }
