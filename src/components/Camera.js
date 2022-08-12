2a1d55a07d2249d860f428c054c07bbd82f1ad8c
jadson <jadson.g-matos@outlook.com>
2022-08-12 00:57:05 +0000

    camera hover move

--- src/components/Camera.js
+++ src/components/Camera.js
@@ -1,7 +1,25 @@
 import { useEffect, useState } from "react";
 
 function Camera() {
+  const [update, set_update] = useState(0);
   const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+
+  const move_can = () => {
+    const size_marginX = (window.visualViewport.width * 10) / 100;
+    const size_marginY = (window.visualViewport.height * 10) / 100;
+
+    if (mousePosition.x < size_marginX) {
+      window.scroll(window.scrollX - 1, window.scrollY);
+    } else if (mousePosition.x > window.visualViewport.width - size_marginX) {
+      window.scroll(window.scrollX + 1, window.scrollY);
+    }
+
+    if (mousePosition.y < size_marginY) {
+      window.scroll(window.scrollX, window.scrollY - 1);
+    } else if (mousePosition.y > window.visualViewport.height - size_marginY) {
+      window.scroll(window.scrollX, window.scrollY + 1);
+    }
+  };
 
   useEffect(() => {
     console.log(
@@ -16,38 +34,21 @@ function Camera() {
 
     const setFromEvent = (e) => {
       //console.log(e)
-      return setMousePosition({ x: e.x, y: e.y });
+      setMousePosition({ x: e.x, y: e.y });
     };
+    
     window.addEventListener("mousemove", setFromEvent);
     return () => {
       window.removeEventListener("mousemove");
     };
   }, []);
 
-  function move_can(time) {
-    const size_marginX = (window.visualViewport.width * 10) / 100;
-    const size_marginY = (window.visualViewport.height * 10) / 100;
-
-    if (mousePosition.x < size_marginX) {
-      window.scroll(window.scrollX - 1, window.scrollY);
-      setTimeout(move_can(time + 1000), time);
-    } else if (mousePosition.x > window.visualViewport.width - size_marginX) {
-      window.scroll(window.scrollX + 1, window.scrollY);
-      setTimeout(move_can(time + 1000), time);
-    }
-
-    if (mousePosition.y < size_marginY) {
-      window.scroll(window.scrollX, window.scrollY - 1);
-      setTimeout(move_can(time + 1000), time);
-    } else if (mousePosition.y > window.visualViewport.height - size_marginY) {
-      window.scroll(window.scrollX, window.scrollY + 1);
-      setTimeout(move_can(time + 1000), time);
-    }
-  }
-
   useEffect(() => {
-    move_can();
-  }, [mousePosition]);
+    setTimeout(() => {
+      set_update(update + 1 % 3);
+      move_can();
+    }, 16);
+  }, [update]);
 }
 
 export default Camera;
