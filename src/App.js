2be255ae1041f6599f631b4fd4f8f3c607bd79cf
jadson <jadson.g-matos@outlook.com>
2022-08-16 18:25:22 +0000

    click_map

--- src/App.js
+++ src/App.js
@@ -3,48 +3,20 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import { useSelector, useDispatch } from "react-redux";
 
-//import { Counter } from "./features/counter/Counter";
 import { Player } from "./features/player/Player";
 import Camera from "./components/Camera";
 import Mapa from "./components/Mapa";
 
-import {
-  set_x,
-  set_y,
-  set_style_img,
-  select_all,
-} from "./features/player/slice";
 
 function App() {
-  const player_state = useSelector(select_all);
-  const dispatch = useDispatch();
-
-  function click_map(e) {
-    console.log("click_map", player_state);
-
-    const x =
-      e.clientX - player_state.size_half + window.visualViewport.pageLeft;
-    const y = e.clientY - player_state.size + window.visualViewport.pageTop;
-
-    console.log(x, y);
-
-    dispatch(set_x(x));
-    dispatch(set_y(y));
-
-    dispatch(
-      set_style_img({
-        transform: `translate(${x}px,${y}px)`,
-      })
-    );
-  }
-
   return (
     <div>
+      <Camera />
       <header>
         <p>header</p>
       </header>
       <div>
-        <Mapa nClick={click_map} />
+        <Mapa />
 
         <Player></Player>
       </div>
--- src/components/Mapa.js
+++ src/components/Mapa.js
@@ -1,8 +1,40 @@
 import "./Mapa.css";
+import { useSelector, useDispatch } from "react-redux";
+
+import {
+  set_x,
+  set_y,
+  set_style_img,
+  select_all,
+} from "../features/player/slice";
 
 function Mapa() {
+
+  const player_state = useSelector(select_all);
+  const dispatch = useDispatch();
+
+  function click_map(e) {
+    
+    console.log("click_map", player_state);
+
+    const x =
+      e.clientX - player_state.size_half + window.visualViewport.pageLeft;
+    const y = e.clientY - player_state.size + window.visualViewport.pageTop;
+
+    console.log(x, y);
+
+    dispatch(set_x(x));
+    dispatch(set_y(y));
+
+    dispatch(
+      set_style_img({
+        transform: `translate(${x}px,${y}px)`,
+      })
+    );
+  }
+
   return (
-    <div className="map">
+    <div className="map" onClick={click_map} >
       <img
         draggable="false"
         className="map-b position-absolute top-50 start-50 translate-middle"
